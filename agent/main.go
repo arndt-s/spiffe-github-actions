6da4c89a7fd8db00a6c1c36c47681acbc0014bba
Arndt Schwenkschuster <17650715+arndt-s@users.noreply.github.com>
2025-01-20 16:50:35 +0000

    add simple agent

---
+++ agent/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/spiffe/go-spiffe/proto/spiffe/workload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
+)
+
+func main() {
+	s := grpc.NewServer()
+	a, err := newAgent()
+	if err != nil {
+		panic(fmt.Errorf("failed to create agent: %v", err))
+	}
+
+	reflection.Register(s)
+	workload.RegisterSpiffeWorkloadAPIServer(s, a)
+
+	path := os.Getenv("SPIFFE_ENDPOINT_SOCKET")
+	if path == "" {
+		panic("SPIFFE_ENDPOINT_SOCKET is not set")
+	}
+
+	l, err := net.Listen("unix", path)
+	if err != nil {
+		panic(fmt.Errorf("failed to listen: %v", err))
+	}
+
+	fmt.Printf("Listening on %s\n", l.Addr().String())
+	err = s.Serve(l)
+	if err != nil {
+		panic(fmt.Errorf("failed to serve: %v", err))
+	}
+}
+
+type agent struct {
+	signer jose.Signer
+	workload.UnimplementedSpiffeWorkloadAPIServer
+}
+
+func newAgent() (*agent, error) {
+	signingKey := []byte("secret")
+	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.HS256, Key: signingKey}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &agent{
+		signer: signer,
+	}, nil
+}
+
+func (a *agent) FetchJWTSVID(ctx context.Context, req *workload.JWTSVIDRequest) (*workload.JWTSVIDResponse, error) {
+	claims := jwt.Claims{
+		Expiry:  jwt.NewNumericDate(time.Now().Add(time.Hour * 1)),
+		Issuer:  "your-issuer",
+		Subject: "your-subject",
+	}
+
+	raw, err := jwt.Signed(a.signer).Claims(claims).CompactSerialize()
+	if err != nil {
+		return nil, err
+	}
+
+	return &workload.JWTSVIDResponse{
+		Svids: []*workload.JWTSVID{
+			{
+				Svid:     raw,
+				SpiffeId: "",
+			},
+		},
+	}, nil
+}
